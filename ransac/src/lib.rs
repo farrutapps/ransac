3ff92f3b790f794fd96e1992b824758256bb0db6
Sebastian Ratz <sebastian.ratz.s@gmail.com>
2022-11-26 12:34:48 +0100

    reformat

--- ransac/src/lib.rs
+++ ransac/src/lib.rs
@@ -1,7 +1,7 @@
 pub mod model;
 use model::Model;
 pub mod line2d;
-use line2d::{Point2d, Line2d};
+use line2d::{Line2d, Point2d};
 use rand::{seq::IteratorRandom, thread_rng};
 use std::cmp;
 
@@ -9,7 +9,7 @@ pub struct RansacSettings<M: Model> {
     pub max_inlier_error: M::Error,
     pub max_iters: i32,
     pub expected_inlier_ratio: f64,
-    pub convergence_probability: f64
+    pub convergence_probability: f64,
 }
 
 pub fn run_ransac<M: Model>(data: &Vec<M::DataPoint>, params: &RansacSettings<M>) -> Option<M> {
@@ -22,18 +22,21 @@ pub fn run_ransac<M: Model>(data: &Vec<M::DataPoint>, params: &RansacSettings<M>
         return None;
     }
 
-    let num_iters = ((1. - params.convergence_probability).ln() / (1.-params.expected_inlier_ratio.powi(2)).ln()).ceil() as i32;
+    let num_iters = ((1. - params.convergence_probability).ln()
+        / (1. - params.expected_inlier_ratio.powi(2)).ln())
+    .ceil() as i32;
     for _i in 0..cmp::min(num_iters, params.max_iters) {
-
         let sample = data.iter().choose_multiple(&mut rng, 2);
         let hypothesis_model = M::hypothesis(&sample);
-        let num_inliers = data.iter().filter(|pt| hypothesis_model.is_inlier(&pt, &params.max_inlier_error)).count();
+        let num_inliers = data
+            .iter()
+            .filter(|pt| hypothesis_model.is_inlier(&pt, &params.max_inlier_error))
+            .count();
 
         if num_inliers > best_num_inliers {
             best_num_inliers = num_inliers;
             best_model = Some(hypothesis_model);
         }
-
     }
     best_model
 }
--- ransac/src/line2d.rs
+++ ransac/src/line2d.rs
@@ -1,22 +1,25 @@
 use crate::model;
 pub struct Point2d {
     pub x: f64,
-    pub y: f64
+    pub y: f64,
 }
 
 impl Point2d {
     fn distance_to(&self, ln: &Line2d) -> f64 {
-        let x = ((self.x + ln.k*self.y) - ln.k*ln.d) / (ln.k*ln.k + 1.);
-        let y = (ln.k*(self.x + ln.k*self.y) + ln.d) / (ln.k*ln.k + 1.);
-        distance(self,&Point2d{x, y})
+        let x = ((self.x + ln.k * self.y) - ln.k * ln.d) / (ln.k * ln.k + 1.);
+        let y = (ln.k * (self.x + ln.k * self.y) + ln.d) / (ln.k * ln.k + 1.);
+        distance(self, &Point2d { x, y })
     }
 
     fn subtr(&self, rhs: &Point2d) -> Point2d {
-        Point2d{x: self.x - rhs.x, y: self.y - rhs.y}
+        Point2d {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
     }
 
     fn norm(&self) -> f64 {
-        f64::sqrt(self.x*self.x + self.y*self.y)
+        f64::sqrt(self.x * self.x + self.y * self.y)
     }
 }
 
@@ -27,14 +30,14 @@ fn distance(pt1: &Point2d, pt2: &Point2d) -> f64 {
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Line2d {
     pub k: f64,
-    pub d: f64
+    pub d: f64,
 }
 
 impl Line2d {
     fn from_points(pt1: &Point2d, pt2: &Point2d) -> Self {
         let k = (pt2.y - pt1.y) / (pt2.x - pt1.x);
-        let d = pt1.y - k*pt1.x;
-        return Line2d{k,d};
+        let d = pt1.y - k * pt1.x;
+        return Line2d { k, d };
     }
 }
 
@@ -47,6 +50,6 @@ impl model::Model for Line2d {
     }
 
     fn is_inlier(&self, data_point: &Self::DataPoint, max_inlier_error: &Self::Error) -> bool {
-        data_point.distance_to(&self) < *max_inlier_error 
+        data_point.distance_to(&self) < *max_inlier_error
     }
 }
